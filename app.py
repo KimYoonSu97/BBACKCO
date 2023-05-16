e4977a5b7d52e8657762dc89378e6859799d0108
KimYoonSu97 <zkzk625@naver.com>
2023-05-16 15:38:20 +0900

    app.py 수정

--- app.py
+++ app.py
@@ -1,19 +1,29 @@
 from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-	return render_template('index.html')
+from pymongo import MongoClient
+import certifi
 
-@app.route("/movie", methods=["POST"])
-def movie_post():
-	sample_receive = request.form['sample_give']
-	print(sample_receive)
-	return jsonify({'msg':'POST 연결 완료!'})
+ca = certifi.where()
 
-@app.route("/movie", methods=["GET"])
-def movie_get():
-	return jsonify({'msg':'GET 연결 완료!'})
+# 김윤수의 몽고디비에 연결되어있읍니다.
+client = MongoClient('mongodb+srv://starta:test@cluster0.0ozcuu6.mongodb.net/?retryWrites=true&w=majority', tlsCAFile=ca)
+db = client.dbsparta
 
-if __name__ == '__main__':
-	app.run('0.0.0.0', port=5001, debug=True)
+# 저장 - 예시
+# doc = {'name':'bobby','age':21}
+# db.users.insert_one(doc)
+
+# 한 개 찾기 - 예시
+# user = db.users.find_one({'name':'bobby'})
+
+# 여러개 찾기 - 예시 ( _id 값은 제외하고 출력)
+# all_users = list(db.users.find({},{'_id':False}))
+
+# 바꾸기 - 예시
+# db.users.update_one({'name':'bobby'},{'$set':{'age':19}})
+
+# 지우기 - 예시
+# db.users.delete_one({'name':'bobby'})
+
+# 여기서부터 설명하고 코드를 작성해 주세요!!!
