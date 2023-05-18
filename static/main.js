c8e17ad61a13969bdecc86a16e94f3f155e66d06
KimYoonSu97 <zkzk625@naver.com>
2023-05-18 12:15:16 +0900

    진철님 코드 반영본

--- static/main.js
+++ static/main.js
@@ -1,7 +1,6 @@
 $(document).ready(function () {
   comment();
   member();
-  close_box();
 });
 
 function member() {
@@ -24,38 +23,42 @@ function member() {
 
         let temp_html = `
       
-      <div id="${name}" class="member-card">
-      <div class="member-inner">
-        <h2>${name}</h2>
-        <div class="member-box">    
-          <div class="imgContainer">
-          <img src="${img}" alt="${name}">
-          </div>
-          <div class="member-txt-box">
-            <div class="member-info">
-              <h4>자기소개</h4>
-              <p class="white-box">
-                ${selfIntro}
-              </p>
-            </div>
-            <div class="member-strength">
-              <h4>자신의 장점</h4>
-              <p class="white-box">
-                ${merits}
-              </p>
-            </div>
-            <div class="member-style">
-              <h4>협업 스타일</h4>
-              <p class="white-box">
-              ${style}
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+                <li id="${name}" class="slideitem">
+                    <a>
+                        <section id="member">
+                            <div id="kys">
+                                <div class="member-inner">
+                                    <h2>${name}</h2>
+                                    <div class="member-box">
+                                        <div class="member-img">
+                                             <img src="${img}" alt="${name}">
+                                        </div>
+                                        <div class="member-txt-box">
+                                            <div class="member-info">
+                                                <h3>자기소개</h3>
+                                                <p class="white-box">
+                                                    ${selfIntro}
+                                                </p>
+                                            </div>
+                                            <div class="member-strength">
+                                                <h3>자신의 장점</h3>
+                                                <p class="white-box">
+                                                     ${merits}
+                                                </p>
+                                            </div>
+                                            <div class="member-style">
+                                                <h3>협업 스타일(동물)</h3>
+                                                <p class="white-box">${style}</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </section>
+                    </a>
+                </li>
     `;
-        $("#member").append(temp_html);
+        $(".slidelist").append(temp_html);
       });
     });
 }
@@ -86,52 +89,21 @@ function comment() {
     });
 }
 
-//  폼 작성(댓글) 데이터 저장하는 코드
-//  공유하기 유효성 검사 추가하였음
 function save_form() {
-  let checkName = $("#name");
-  let checkFire = $("#fire");
-  let checkComment = $("#comment");
+  let name = $("#name").val();
+  let fire = $("#fire").val();
+  let comment = $("#comment").val();
 
-  if (!checkName.val()) {
-    //  frn의 product의 value값이 없을 때 = input에 입력한 값이 없을 때
-    alert("상호명을 입력해 주세요");
-    checkName.focus();
-    return false; //  경고창을 확인한 후 페이지가 넘어가지 않고 그대로 유지하기 위함, method빼면 못넘어감.
-  } else if (checkFire.val() == 0) {
-    alert("별점을 선택해 주세요");
-    checkFire.focus();
-    return false;
-  } else if (!checkComment.val()) {
-    alert("코멘트를 입력해 주세요");
-    checkComment.focus();
-    return false;
-  } else {
-    let name = $("#name").val();
-    let fire = $("#fire").val();
-    let comment = $("#comment").val();
+  let formData = new FormData();
+  formData.append("name_give", name);
+  formData.append("comment_give", comment);
+  formData.append("fire_give", fire);
 
-    let formData = new FormData();
-    formData.append("name_give", name);
-    formData.append("comment_give", comment);
-    formData.append("fire_give", fire);
-
-    fetch("/comments", { method: "POST", body: formData })
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data["msg"]);
-        alert(data["msg"]);
-        window.location.reload();
-      });
-  }
-}
-
-// 빡코더 되기 열기 버튼
-function open_box() {
-  $("#post-box").show();
-}
-
-// 빡코더 되기 닫기 버튼
-function close_box() {
-  $("#post-box").hide();
+  fetch("/comments", { method: "POST", body: formData })
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data["msg"]);
+      alert(data["msg"]);
+      window.location.reload();
+    });
 }
